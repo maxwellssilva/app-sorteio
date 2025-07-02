33fc8882e9e17a170f102569ebada9877e34c6f8
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-07-02 14:23:15 -0400

    Initial Commit

---
+++ app-sorteio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-sorteio
+//
+//  Created by Maxwell Silva on 02/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
